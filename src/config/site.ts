081f2bb1b278c906b8610910c04af4cb64f932ee
SID AG <agsid@gmail.com>
2026-03-07 19:51:03 -0500

    v0.1

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 import ogImage from "@/assets/og-image.png";
 
 export const siteConfig = {
-  name: "Astro Starter Pro",
+  name: "Tutoring Website",
   description:
     "Starter template optimized for SEO and performance. A solid foundation to start your projects with best practices.",
   url: "https://astrostarterpro.com",
@@ -18,9 +18,6 @@ export const siteConfig = {
   navLinks: [
     { text: "Home", href: "/" },
     { text: "About", href: "/about" },
-    { text: "Services", href: "/services" },
-    { text: "Blog", href: "/blog" },
     { text: "Contact", href: "/contact" },
-    { text: "Widgets", href: "/widgets" },
   ],
 };
